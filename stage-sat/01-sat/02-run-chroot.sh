c6f883e5ccbf406864d1539ce0bd3261e1fac274
builderjer <builderjer@gmail.com>
2023-07-23 18:03:33 -0600

    fix ovos-audio branch

--- stage-sat/01-sat/02-run-chroot.sh
+++ stage-sat/01-sat/02-run-chroot.sh
@@ -2,4 +2,4 @@ pip3 install git+https://github.com/JarbasHiveMind/HiveMind-voice-sat
 pip3 install git+https://github.com/JarbasHiveMind/hivemind_websocket_client
 pip3 install git+https://github.com/JarbasHiveMind/poorman_handshake
 
-pip3 install git+https://github.com/OpenVoiceOS/ovos-audio@feat/optional_ocp
+pip3 install git+https://github.com/OpenVoiceOS/ovos-audio@bfc259d48c6d221c110b16d7c1c4f33b7ba7471a
