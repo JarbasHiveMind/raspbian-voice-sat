049277d384504e8071672d256ad13c116567b3d6
builderjer <builderjer@gmail.com>
2023-09-03 10:44:06 -0600

    fix missing plugins

--- stage-sat/01-sat/02-run-chroot.sh
+++ stage-sat/01-sat/02-run-chroot.sh
@@ -2,4 +2,7 @@ pip3 install git+https://github.com/JarbasHiveMind/HiveMind-voice-sat
 pip3 install git+https://github.com/JarbasHiveMind/hivemind_websocket_client
 pip3 install git+https://github.com/JarbasHiveMind/poorman_handshake
 
-pip3 install git+https://github.com/OpenVoiceOS/ovos-audio@bfc259d48c6d221c110b16d7c1c4f33b7ba7471a
+pip3 install git+https://github.com/OpenVoiceOS/ovos-audio
+
+pip3 install git+https://github.com/OpenVoiceOS/ovos-tts-plugin-piper
+pip3 install git+https://github.com/OpenVoiceOS/ovos-vad-plugin-silero
