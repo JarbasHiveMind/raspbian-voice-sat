96f7d94860df663456b0a8be8dc6e95e1e32c66c
builderjer <builderjer@gmail.com>
2023-09-03 11:32:17 -0600

    missed ww and tflite

--- stage-sat/01-sat/02-run-chroot.sh
+++ stage-sat/01-sat/02-run-chroot.sh
@@ -4,5 +4,8 @@ pip3 install git+https://github.com/JarbasHiveMind/poorman_handshake
 
 pip3 install git+https://github.com/OpenVoiceOS/ovos-audio
 
+pip install -U -f 'https://whl.smartgic.io/' tflite_runtime
+pip install git+https://github.com/OpenVoiceOS/ovos-ww-plugin-precise-lite
+
 pip3 install git+https://github.com/OpenVoiceOS/ovos-tts-plugin-piper
 pip3 install git+https://github.com/OpenVoiceOS/ovos-vad-plugin-silero
