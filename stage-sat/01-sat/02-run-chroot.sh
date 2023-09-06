5bf4c0c5f945a1a203cce8876e55234480b3af25
builderjer <builderjer@gmail.com>
2023-09-06 05:48:50 -0600

    added ww and tflite

--- stage-sat/01-sat/02-run-chroot.sh
+++ stage-sat/01-sat/02-run-chroot.sh
@@ -2,7 +2,11 @@ pip3 install git+https://github.com/JarbasHiveMind/HiveMind-voice-sat
 pip3 install git+https://github.com/JarbasHiveMind/hivemind_websocket_client
 pip3 install git+https://github.com/JarbasHiveMind/poorman_handshake
 
+pip3 install -U -f 'https://whl.smartgic.io/' tflite_runtime
+
 pip3 install git+https://github.com/OpenVoiceOS/ovos-audio
 
 pip3 install git+https://github.com/OpenVoiceOS/ovos-tts-plugin-piper
 pip3 install git+https://github.com/OpenVoiceOS/ovos-vad-plugin-silero
+
+pip3 install git+https://github.com/OpenVoiceOS/ovos-ww-plugin-precise-lite
