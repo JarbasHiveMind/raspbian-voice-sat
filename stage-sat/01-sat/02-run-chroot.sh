bed2f1c480b8d7c467af08cda53ad76abc79082d
builderjer <builderjer@gmail.com>
2023-09-10 07:33:36 -0600

    custom whl for cryptography

--- stage-sat/01-sat/02-run-chroot.sh
+++ stage-sat/01-sat/02-run-chroot.sh
@@ -1,15 +1,10 @@
-# rust stuff
-wget https://static.rust-lang.org/rustup/dist/armv7-unknown-linux-gnueabihf/rustup-init
-chmod +x rustup-init
-./rustup-init -y
-rm rustup-init
-
-pip3 install -U pip
-
-pip3 install git+https://github.com/pyca/cryptography
+pip3 install -U -f 'https://whl.smartgic.io/' tflite_runtime
+pip3 install -U -f 'https://wheel-index.linuxserver.io/alpine/' cryptography
 
 pip3 install git+https://github.com/JarbasHiveMind/HiveMind-voice-sat
 pip3 install git+https://github.com/JarbasHiveMind/hivemind_websocket_client
 pip3 install git+https://github.com/JarbasHiveMind/poorman_handshake
 
-pip3 install git+https://github.com/OpenVoiceOS/ovos-audio@bfc259d48c6d221c110b16d7c1c4f33b7ba7471a
+pip3 install git+https://github.com/OpenVoiceOS/ovos-audio
+
+pip3 install git+https://github.com/OpenVoiceOS/ovos-ww-plugin-precise-lite
