af3271482553a01bbf1fd5a36d33acc583b6cf9b
builderjer <builderjer@gmail.com>
2023-09-16 14:47:50 -0600

    maybe ggwave

--- stage-ggwave/03-ggwave/02-run-chroot.sh
+++ stage-ggwave/03-ggwave/02-run-chroot.sh
@@ -3,7 +3,7 @@ git clone https://github.com/ggerganov/ggwave --recursive
 cd ggwave && mkdir build && cd build
 cmake .. && make
 
-mv build/bin/* /home/ovos/.local/bin/
+mv /home/ovos/ggwave/build/bin/* /home/ovos/.local/bin/
 rm -rf ggwave
 
 pip3 install git+https://github.com/JarbasHiveMind/hivemind-ggwave
